3a033e363f4bb8acf7e753119052c4df78be31f3
jinseokYeom <jinyeom95@gmail.com>
2016-07-06 15:43:32 -0500

    progress

---
+++ dna.go
@@ -0,0 +1,57 @@
+package eago
+
+import (
+	"bytes"
+	"math/rand"
+)
+
+type DNA struct {
+	size int    // size of the chromosome
+	gene string // binary-coded gene
+}
+
+func NewDNA(size int) *DNA {
+	return &DNA{
+		size: size,
+		gene: func() string {
+			var buf bytes.Buffer
+			for i := 0; i < size; i++ {
+				if rand.Float64() < 0.5 {
+					buf.WriteString("1")
+				} else {
+					buf.WriteString("0")
+				}
+			}
+			return buf.String()
+		}(),
+	}
+}
+
+// get size
+func (d *DNA) Size() int {
+	return d.size
+}
+
+// get gene
+func (d *DNA) Gene() string {
+	return d.gene
+}
+
+// bit flip mutation given mutation rate
+func (d *DNA) BFMutate(r float64) {
+	var newGene bytes.Buffer
+	for i := 0; i < d.size; i++ {
+		bit := string(d.gene[i])
+		if rand.Float64() > r {
+			newGene.WriteString(bit)
+		} else {
+			if bit == "0" {
+				newGene.WriteString("1")
+			} else {
+				newGene.WriteString("0")
+			}
+		}
+	}
+	// update gene
+	d.gene = newGene.String()
+}
---
+++ dna_test.go
@@ -0,0 +1,31 @@
+package eago
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+func TestDNA(t *testing.T) {
+	// rand seed set to 0
+	rand.Seed(0)
+
+	// create a new DNA
+	d := NewDNA(10)
+	fmt.Printf("DNA size: %d\n", d.Size())
+	fmt.Printf("DNA gene: %s\n", d.Gene())
+
+	// rand seed set to 1
+	rand.Seed(1)
+
+	// create a new DNA
+	d = NewDNA(10)
+	fmt.Printf("DNA size: %d\n", d.Size())
+	fmt.Printf("DNA gene: %s\n", d.Gene())
+
+	// bit flip mutation
+	mrate := 0.3
+	d.BFMutate(mrate)
+	fmt.Printf("Mutation rate: %f\n", mrate)
+	fmt.Printf("Mutated DNA gene: %s\n", d.gene)
+}
