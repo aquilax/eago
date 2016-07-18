d4b37079f84f5b0b594d5c1e06db03c84a66cdc9
jinseokYeom <jinyeom95@gmail.com>
2016-07-17 22:46:10 -0500

    selection functions / godoc ready

--- selection.go
+++ selection.go
@@ -6,37 +6,43 @@ import "math/rand"
 // parent DNA from a population given a comparison function.
 type SelectFunc func(CompareFunc, []*DNA) *DNA
 
-// Tournament Selection
-func TSelect(c CompareFunc, p []*DNA) *DNA {
-	popSize := len(p)
-	best := rand.Intn(popSize)
-	for i := 0; i < popSize; i++ {
-		next := rand.Intn(popSize)
-		// if next DNA is superior
-		if c(p[next], p[best]) == 1 {
-			best = next
+// TSelect() returns a selection function that performs
+// Tournament Selection.
+func TSelect() SelectFunc {
+	return func(c CompareFunc, p []*DNA) *DNA {
+		popSize := len(p)
+		best := rand.Intn(popSize)
+		for i := 0; i < popSize; i++ {
+			next := rand.Intn(popSize)
+			// if next DNA is superior
+			if c(p[next], p[best]) == 1 {
+				best = next
+			}
 		}
+		return p[best]
 	}
-	return p[best]
 }
 
-// Fitness-Proportionate Selection (not recommended)
-func FPSelect(c CompareFunc, p []*DNA) *DNA {
-	popSize := len(p)
-	best := p[rand.Intn(popSize)]
-	bestScore := best.Fitness()
-	for i := 0; i < popSize; i++ {
-		if c(p[i], best) == 1 {
-			best.Copy(p[i])
-			bestScore = p[i].Fitness()
+// FPSelect() returns a selection function that performs
+// Fitness-Proportionate Selection (not recommended).
+func FPSelect() SelectFunc {
+	return func(c CompareFunc, p []*DNA) *DNA {
+		popSize := len(p)
+		best := p[rand.Intn(popSize)]
+		bestScore := best.Fitness()
+		for i := 0; i < popSize; i++ {
+			if c(p[i], best) == 1 {
+				best.Copy(p[i])
+				bestScore = p[i].Fitness()
+			}
 		}
-	}
-	// stochastic acceptance
-	for {
-		i := rand.Intn(popSize)
-		r := p[i].Fitness() / bestScore
-		if rand.Float64() < r {
-			return p[i]
+		// stochastic acceptance
+		for {
+			i := rand.Intn(popSize)
+			r := p[i].Fitness() / bestScore
+			if rand.Float64() < r {
+				return p[i]
+			}
 		}
 	}
 }
