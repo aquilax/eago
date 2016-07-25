1c2b888d385cc66263f296dcb7377b2ad8810ed6
jinseokYeom <jinyeom95@gmail.com>
2016-07-25 16:02:47 -0500

    progress

--- ne/neuralnetwork.go
+++ ne/neuralnetwork.go
@@ -1,11 +1,46 @@
 package ne
 
-type NeuralNet struct {
-	numInput  int // number of inputs
-	numOutput int // number of outputs
+import (
+	"log"
 
+	"github.com/jinseokYeom/eago"
+)
+
+const (
+	BIAS = -1.0 // bias for activation
+)
+
+type NeuralNet struct {
+	conf    *Config   // neural net configuration
+	weights []float64 // weights for activation
 }
 
-func NewNeuralNet() *NeuralNet {
-	return &NeuralNet{}
+// Create a new neural network with 0 weights.
+func NewNeuralNet(conf *Config) *NeuralNet {
+	return &NeuralNet{
+		conf: conf,
+		weights: func(c *Config) []float64 {
+			// number of weights including bias
+			numWeights := (c.NumInputs+1)*c.NumNeurons +
+				(c.NumLayers-1)*(c.NumNeurons+1)*c.NumNeurons +
+				(c.NumNeurons+1)*conf.NumOutputs
+			weights := make([]float64, numWeights)
+			return weights
+		}(conf),
+	}
+}
+
+// Build weights by decoding a DNA and
+// generate neural network's weights.
+func (n *NeuralNet) Build(d *eago.DNA) {
+	// if DNA size cannot be divided by number of weights
+	if d.Size()%len(n.weights) != 0 {
+		log.Fatal("Invalid DNA size")
+	}
+	n.weights = n.decode(d)
+}
+
+// Decode a DNA and generate float64 slice.
+func (n *NeuralNet) decode(d *eago.DNA) []float64 {
+
 }
