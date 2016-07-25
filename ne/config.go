cabb5c7d2655b73b3327b8487dbc1c38cbf52db3
jinseokYeom <jinyeom95@gmail.com>
2016-07-25 16:02:33 -0500

    progress

---
+++ ne/config.go
@@ -0,0 +1,24 @@
+package ne
+
+type Config struct {
+	NumInputs  int            // number of inputs
+	NumOutputs int            // number of outputs
+	NumLayers  int            // number of hidden layers
+	NumNeurons int            // number of hidden neurons
+	Activation ActivationFunc // activation function
+}
+
+// Create a new configuration set to null.
+func NewConfig() *Config {
+	return &Config{}
+}
+
+// Set number of inputs.
+func (c *Config) SetNumInputs(n int) {
+	c.NumInputs = n
+}
+
+// Set number of outputs.
+func (c *Config) SetNumOutputs(n int) {
+	c.NumOutputs = n
+}
