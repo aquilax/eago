5124d9d2cfa963760a67844ccfd195297df8cc3a
jinseokYeom <jinyeom95@gmail.com>
2016-07-28 15:10:53 -0500

    minor fix

--- ea_test.go
+++ ea_test.go
@@ -12,7 +12,7 @@ func Eval(d *DNA) float64 {
 	for i := 0; i < size; i++ {
 		bit := string(gene[i])
 		if bit == "1" {
-			score++
+			score += 1.0
 		}
 	}
 	return score
@@ -23,12 +23,12 @@ func TestGA(t *testing.T) {
 	c := &Config{
 		GeneLen:      30,
 		PopSize:      100,
-		NumGen:       500,
-		MutationRate: 0.5,
-		Compare:      DirectCompare,
+		NumGen:       100,
+		MutationRate: 0.3,
+		Compare:      DirectCompare(),
 		Evaluate:     Eval,
-		Select:       TSelect,
-		Crossover:    Crossover1P,
+		Select:       TSelect(),
+		Crossover:    UCrossover(0.3),
 	}
 
 	ga := NewGA(c)
