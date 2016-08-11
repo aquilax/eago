a6dd76ac7155027523374780101477a6ee8c52c1
jinseokYeom <jinyeom95@gmail.com>
2016-08-11 12:56:17 -0400

    initial commit

---
+++ coop.go
@@ -0,0 +1,16 @@
+package eago
+
+// Cooperative Coevolution
+type CoopCoevolution struct {
+	numPopulation int  // number of populations
+	populations   []EA // list of populations
+}
+
+func NewCoopCoevolution() CoopCoevolution {
+	return &CoopCoevolution{
+		numPopulation: n,
+		population: func() []EA {
+
+		}(),
+	}
+}
